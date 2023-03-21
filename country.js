57fff18c9830d1ecdb4ded8a50e78a3887283941
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-21 17:12:00 +0600

    object destructuring

--- country.js
+++ country.js
@@ -1,27 +1,37 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => displayCountries(data))
+        .then(res => res.json())
+        .then(data => displayCountries(data))
 
 }
 
 loadCountries();
 
 const displayCountries = countries => {
-   const countryHTML = countries.map(country => getCountryHtml(country));
-   console.log(countries[0]);
-   const container = document.getElementById('container');
-   container.innerHTML = countryHTML.join(' ');
-//    console.log(countryHTML.join(' '));
+    console.log(countries[6]);
+    const countryHTML = countries.map(country => getCountryHtml(country));
+       
+
+
+    const container = document.getElementById('container');
+    container.innerHTML = countryHTML.join(' ');
 }
 
 // getCounty function declaration
+// option-2
 
-const getCountryHtml = country =>{
+// destructuring
+const getCountryHtml = ({name, flags, area, idd}) => {
+    // // option -1
+    // const { name, flags } = country;
+    // console.log(name);
+
     return `
     <div>
-    <h2>${country.name.common}</h2>
-    <img src="${country.flags.png}">
+    <h2>${name.common}</h2>
+    <p>Area: ${area} </p>
+    <p>Car: ${idd.root} </p>
+    <img src="${flags.png}">
     </div>
     `
 }
