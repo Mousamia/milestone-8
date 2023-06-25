b2cf54fccbdb3b2bed88022b3344370fb03ce4d9
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-06-25 23:02:09 +0600

    data brought

---
+++ name-of-your-project/src/Comonents/Food.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Food.css';
+
+const Food = () => {
+    return (
+        <div>
+          <h1> Food Menu</h1>
+        </div>
+    );
+};
+
+export default Food;
