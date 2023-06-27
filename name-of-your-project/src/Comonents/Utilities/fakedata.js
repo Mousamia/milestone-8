28a3b8e5254039fe8d790868e46bcfafcb5fb803
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-06-27 23:55:12 +0600

    multiple object sving

--- name-of-your-project/src/Comonents/Utilities/fakedata.js
+++ name-of-your-project/src/Comonents/Utilities/fakedata.js
@@ -1,15 +1,23 @@
 /* eslint-disable no-unused-vars */
 // manage localstorge
 const addToLoclStorge = id => {
+    // it will give the value of id
     const kuntity = localStorage.getItem(id);
     console.log(kuntity);
-    console.log(typeof(kuntity));
-    if(kuntity == 1){
-        console.log("poimout")
+
+    let shoppingCart;
+   
+   
+    if(kuntity){
+      let newKuntity = parseInt(kuntity) + 1;
+      localStorage.setItem(id, newKuntity);
+
+
     }
-    else 
-    console.log("dhr mi dhrin");
-    localStorage.setItem(id, 1);
+    else {
+        localStorage.setItem(id, 1);
+        console.log("one item added");
+    }
 }
 
 export { addToLoclStorge };
